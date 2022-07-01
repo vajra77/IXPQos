5f6b85900a797304a50b821e390474f13c99927f
vajra77 <f.ferreri@namex.it>
2022-07-01 22:11:09 +0200

    minor changes

--- ixpqos/app/config.py
+++
@@ -1,8 +0,0 @@
-APP_CONFIG = {
-    "dbhost": "sys.namex.it",
-    "dbport": 8086,
-    "dbname": "ixpqos_devel",
-    "logfile": "app.log",
-    "targets": "../assets/targets.json",
-    "schedule": "../assets/schedule.json"
-}
